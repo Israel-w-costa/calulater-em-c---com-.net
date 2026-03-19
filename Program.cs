8096fcb47ba4a6c018ead1cfa661fe1ceb427b6d
israel <israelwendellc@gmail.com>
2026-03-19 17:12:04 -0300

    calculadora em c# com .net

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+class Calculadora
+{
+  static void Main(String[] args)
+  { 
+    
+   while(true) {
+    int num1, num2, solucao;
+
+    Console.Write("=======  Bem vindo a Calculadora  ======= \n ");
+    Console.Write("\nDigite um numero para calculo\n");
+    if (!int.TryParse(Console.ReadLine(), out num1))
+            {
+                Console.WriteLine("Entrada inválida!");
+                continue;
+            }
+    
+    Console.Write("\nDigite um outro numero\n");
+    if (!int.TryParse(Console.ReadLine(), out num2))
+            {
+                Console.WriteLine("Entrada inválida!");
+                continue;
+            }
+
+    Console.Write(" Escolha um operador para realizar uma operação");
+    Console.WriteLine("\nEscolha uma operação:");
+            Console.WriteLine("(+) Adição");
+            Console.WriteLine("(-) Subtração");
+            Console.WriteLine("(X) Multiplicação");
+            Console.WriteLine("(/) Divisão");
+            Console.WriteLine("(0) Sair\n");
+    
+    
+    char operacao = char.ToUpper( Console.ReadKey().KeyChar);    if (operacao == '0')
+      {
+        break;
+      }
+      
+
+    switch (operacao)
+    {
+      case '+':
+      solucao = num1 + num2;
+          Console.WriteLine("\n Adição é " + solucao + "\n");
+          break;
+      case'-':
+      solucao = num1 - num2;
+          Console.WriteLine("\n Subtração é " + solucao + "\n");
+          break;    
+      case 'X':
+      solucao = num1 * num2;
+          Console.WriteLine("\n Multiplicação é " + solucao + "\n");
+          break;    
+       case '/':
+       solucao = num1 / num2;
+          if (num2 == 0)
+          {
+            Console.Write("\n Não é possivel dividir por zero");
+          }
+          else
+          {
+            
+          Console.WriteLine("\n Divisão é " + solucao + "\n");
+          }
+          break;   
+      default:
+        Console.Write("opção inválida");    
+        break;
+    }
+}
+  }
+}
